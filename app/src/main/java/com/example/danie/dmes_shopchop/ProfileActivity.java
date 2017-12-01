b1d3baaba262d31951b5dcabf34973d11062586e
michael azarzar <michaelazarzar@gmail.com>
2017-12-01 10:16:38 +0200

    Commiting and pushing profile activity.

--- app/src/main/java/com/example/danie/dmes_shopchop/ProfileActivity.java
+++ app/src/main/java/com/example/danie/dmes_shopchop/ProfileActivity.java
@@ -2,6 +2,8 @@ package com.example.danie.dmes_shopchop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void onTouch(View view) {
+        Toast.makeText(getApplicationContext(), "not implemented yet", Toast.LENGTH_SHORT).show();
     }
 }
